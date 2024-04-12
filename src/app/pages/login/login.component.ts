6946d0773cb99b678c928a4ef8d57bfe4d97eec4
Randy Gomes <raandy.gc@gmail.com>
2024-04-12 03:09:56 -0300

    feat: login - inicio lógica submitLogin

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,47 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginObj: Login;
+
+  constructor(private http: HttpClient){
+    this.loginObj = new Login();
+  }
+
+  // Função Login Usuário
+  submitLogin(){ 
+       
+  }
 }
+
+export class Login{
+  Email: string;
+  password: string;
+
+  constructor(){
+    this.Email = "";
+    this.password = "";
+  }
+
+}
+
+
+// this.http.post('http://localhost:3000/usuarios', this.loginObj).subscribe((res:any)=>{
+//       if (res.result) {
+//         alert("Login realizado!");
+//       } else{
+//         alert(res.message)
+//       }
+//     })
