079d791ed73da33a090cfac005bb739be39e70ed
DyGomesc <raandy.gc@gmail.com>
2024-04-14 14:54:04 -0300

    feat: login - Implementação função submitRegister

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,24 +16,51 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 export class LoginComponent {
 
   loginForm = this.fb.group({
-    email: ['', Validators.required, Validators.email],
-    password: ['', Validators.required, Validators.minLength(6)]
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
   })
-  
-  constructor(private fb:FormBuilder){ }
 
-  // Função Login Usuário
-  submitLogin(){ 
-       
+  registerForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  })
+
+  private usersDB = 'http://localhost:3000/users'
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient,
+  ) { }
+
+
+  // Função realizar Login
+  submitlogin() {
+    
   }
+  // Função realizar Cadastro
+  submitRegister() {
+    if (this.registerForm.value.password === this.registerForm.value.confirmPassword) {
+      const user = {
+        name: this.registerForm.value.name,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password,
+      }
+      this.http.post(this.usersDB, user).subscribe((response) => {
+        alert('Usuário cadastrado com sucesso!');
+        this.router.navigate([''])
+      },
+      (error) => {
+        alert('Erro ao salvar usuário')
+      })
+    } else{
+      alert("As Senhas não conferem!")
+    }
+  }
+
+
+
+
 }
-
-
-
-// this.http.post('http://localhost:3000/usuarios', this.loginObj).subscribe((res:any)=>{
-//       if (res.result) {
-//         alert("Login realizado!");
-//       } else{
-//         alert(res.message)
-//       }
-//     })
