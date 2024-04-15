497604e4f42dde909621a32c1a5adc3b7fbd22f5
Randy Gomes <raandy.gc@gmail.com>
2024-04-15 17:57:06 -0300

    Feature: login - implementação da tela de login/cadastro concluída

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
@@ -14,14 +13,8 @@ export const routes: Routes = [
         component: LoginComponent
     },
     {
-        path:'',
-        component: LayoutComponent,
-        children: [
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            }
-        ]
+        path:'dashboard',
+        component: DashboardComponent
     }
 
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,66 +1,79 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    HttpClientModule,
-    ReactiveFormsModule,
-  ],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  registeredUsers: any[] = [];
 
-  loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  })
+  constructor(private router: Router) { }
 
-  registerForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required],
-  })
-
-  private usersDB = 'http://localhost:3000/users'
-
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private http: HttpClient,
-  ) { }
-
-
-  // Função realizar Login
-  submitlogin() {
-    
-  }
-  // Função realizar Cadastro
-  submitRegister() {
-    if (this.registerForm.value.password === this.registerForm.value.confirmPassword) {
-      const user = {
-        name: this.registerForm.value.name,
-        email: this.registerForm.value.email,
-        password: this.registerForm.value.password,
-      }
-      this.http.post(this.usersDB, user).subscribe((response) => {
-        alert('Usuário cadastrado com sucesso!');
-        this.router.navigate([''])
-      },
-      (error) => {
-        alert('Erro ao salvar usuário')
-      })
-    } else{
-      alert("As Senhas não conferem!")
+  ngOnInit() {
+    const localData = localStorage.getItem('registeredUsers');
+    if (localData != null) {
+      this.registeredUsers = JSON.parse(localData)
     }
-  }
+  };
 
+  loginForm = new FormGroup({
+    emailLogin: new FormControl(''),
+    passwordLogin: new FormControl(''),
+  });
 
+  registerForm = new FormGroup({
+    userNameRegister: new FormControl(''),
+    emailRegister: new FormControl(''),
+    passwordRegister: new FormControl(''),
+    confirmPasswordRegister: new FormControl(''),
+  });
 
+  submitRegister() {
+    if (this.registerForm.value.passwordRegister === this.registerForm.value.confirmPasswordRegister) {
+      const user = {
+        userName: this.registerForm.value.userNameRegister,
+        email: this.registerForm.value.emailRegister,
+        password: this.registerForm.value.passwordRegister,
+        auth: "",
+      }
+
+      const localUser = localStorage.getItem('registeredUsers');
+      if (localUser != null) {
+        const registeredUsers = JSON.parse(localUser);
+        registeredUsers.push(user);
+        localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers))
+      } else {
+        const registeredUsers = [];
+        registeredUsers.push(user);
+        localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers))
+      }
+      alert("Cadastrado com Sucesso!")
+      this.router.navigateByUrl('/login')
+    } else {
+      alert("As senhas não conferem!")
+    }
+  };
+
+  submitLogin() {
+    const userRegistered = this.registeredUsers.find(m => m.email == this.loginForm.value.emailLogin && m.password == this.loginForm.value.passwordLogin);
+
+    if (userRegistered != undefined) {
+      this.router.navigateByUrl('/dashboard')
+      userRegistered.auth = 'logged'
+      alert("Usuário Logado com Sucesso!")
+      localStorage.setItem('registeredUsers', JSON.stringify(userRegistered))
+    } else {
+      alert("Dados de Login incorretos.")
+    }
+  };
+
+  resetPassword() {
+    alert('Função em desenvolvimento!')
+}
 
 }
