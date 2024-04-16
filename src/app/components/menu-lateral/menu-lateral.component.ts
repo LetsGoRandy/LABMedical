1728911f8e66f07eb8636d46d04a4dbbd78fc324
Randy Gomes <raandy.gc@gmail.com>
2024-04-16 00:01:28 -0300

    Feature:menu-lateral - HTML e SCSS

---
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-menu-lateral',
+  standalone: true,
+  imports: [
+    MatIconModule,
+  ],
+  templateUrl: './menu-lateral.component.html',
+  styleUrl: './menu-lateral.component.scss'
+})
+export class MenuLateralComponent {
+
+}
