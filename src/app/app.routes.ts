9207b9bccf6959ceb0c1ded8771c932f799554aa
Randy Gomes <118479151+DyGomesc@users.noreply.github.com>
2024-04-20 18:47:16 -0300

    Feature/rework login (#9)
    * Feature/Rework-login - Criação page Register
    
    * Feature/rework-login - Recriação e separação das telas login e register
    
    * Feature/rework-login - Correção de Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MedicalHistorysComponent } from './pages/medical-historys/medical-historys.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/Home/home.component';
-
 
 export const routes: Routes = [
     {
@@ -14,6 +14,10 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'cadastro',
+        component: RegisterComponent
     },
     {
         path: 'home',
