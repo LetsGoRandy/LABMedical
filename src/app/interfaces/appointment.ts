b2fa73a7534487f49d18632af56532ab2a460b53
Randy Gomes <118479151+DyGomesc@users.noreply.github.com>
2024-04-29 02:27:27 -0300

    Feature/new appointment - Implementação da funcionalidade (#16)
    * Feature/NewAppointment  - Criação Layout e Routes
    
    * Feature/NewAppointment - Implementação da funcionalidade

---
+++ src/app/interfaces/appointment.ts
@@ -0,0 +1,10 @@
+export interface Appointment {
+    id?: string,
+    patientName: string,
+    reason: string,
+    date: string,
+    time: string,
+    description: string,
+    medication: string,
+    precaution: string,
+}
